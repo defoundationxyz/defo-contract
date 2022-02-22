21beb61fc105e76ef171205f175f2f6da0a9f6d8
Dogus Bingul <dogus@defoundation.xyz>
2022-02-22 12:27:36 +0300

    DSC-2 init

--- contracts/DefoErc20.sol
+++ contracts/DefoErc20.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Defo {}
+contract Defo is ERC20, Ownable {
+    constructor() ERC20("Defo Token", "DEFO") {}
+}
