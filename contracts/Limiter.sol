734f1109f83f6c48f8f2e6d605613bcb10af5938
defobari <jabari@defoundation.xyz>
2022-03-23 19:26:58 -0500

    DSC-8 Initial Commit
    Set up the Limiter contract with barebones functions and variables

--- contracts/Limiter.sol
+++ contracts/Limiter.sol
@@ -3,14 +3,19 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//Make the conctract upgradeable
 
 contract Limiter is AccessControl {
+    address public owner;
+    address defoNode;
     mapping(address => uint256) TransferLog;
 
     // minimum allowed time between transfers
     uint256 TimeLimit;
     // tax rate tax only applies to non-whitelisted addresses
-    uint256 TaxRate;
+    uint256 TaxRate = 5000; //basis points
 
     address public TaxCollector;
     //limiter must be pre-approved by taxed token
@@ -21,10 +26,70 @@ contract Limiter is AccessControl {
 
     mapping(address => bool) Blocklist;
 
-    constructor(uint256 _timeLimit, address _taxCollector) {
+    constructor(uint256 _timeLimit, address _taxCollector, address _defoNodeAddress) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        owner = msg.sender;
+        defoNode = _defoNodeAddress;
         TimeLimit = _timeLimit;
         TaxCollector = _taxCollector;
+    }
+
+    modifier onlyNode() {
+        require (
+            address(msg.sender) == defoNode, 
+            "Only Defo node contract can call this function"
+        );
+        _;
+    }
+
+    modifier checkTimeframe() {
+        _;
+    }
+
+    //Can't get the `DEFAULT_ADMIN_ROLE` address from AccessControl so used owner variable
+    // Add Ownable
+    modifier notDenied(
+        address sender,
+        address from,
+        address to,
+        address origin
+
+    ) {
+        if (
+            origin != owner &&
+            to != owner) {
+                require(
+                    !Blocklist[sender] &&
+                    !Blocklist[from] &&
+                    !Blocklist[to] &&
+                    !Blocklist[origin],
+                    "Address is not permitted"
+
+                );
+        }
+        _;
+    }
+
+    event UserLimiterBought ();
+    event UserLimiterSellOrLiquidityAdd ();
+    event UserLimiterLiquidityWithdrawal ();
+    event UserLimiterTransfer ();
+
+    function beforeTokenTrasfer() external onlyNode checkTimeframe /*notDenied()*/ {}
+
+    /// Use basis points for input
+    ///   i.e. if you want 2% input 200, 
+    ///   if you want 20% input 2000
+    function setTaxRate (uint256 newTaxRate) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        TaxRate = newTaxRate;
+    }
+
+    function setTaxCollector (address newTaxCollector) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        TaxCollector = newTaxCollector;
+    }
+
+    function setNodeAddress(address newNodeAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        defoNode = newNodeAddress;
     }
 
     function setToken(address _token) external onlyRole(DEFAULT_ADMIN_ROLE) {
@@ -59,7 +124,7 @@ contract Limiter is AccessControl {
         }
 
         if (TaxedToken != address(0)) {
-            uint256 taxedamount = (amount * TaxRate) / 100;
+            uint256 taxedamount = (amount * TaxRate) / 10000;
             IERC20 token = IERC20(TaxedToken);
             token.transferFrom(from, TaxCollector, taxedamount);
         }
