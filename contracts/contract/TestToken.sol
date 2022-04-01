eed878a833a41f325104036fec714fc386762953
Abhishek kochar <abhishekkochar2@Gmail.com>
2022-04-01 19:49:52 +1300

    Added missing Testtoken file

---
+++ contracts/contract/TestToken.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
+
+//Sole purpose of testing lp manager
+contract TestToken is ERC20{
+    uint256 public _totalSupply = 10000*1e18;
+    
+    constructor(address _owner) ERC20("Test Token","TT"){
+        _mint(_owner, _totalSupply);
+    }
+
+}
