4950b3ef1842a2875a0476799f4ecbe36b45e92f
Dogus Bingul <dogus@defoundation.xyz>
2022-04-27 16:05:15 +0300

    merge fixes

--- contracts/interfaces/ILpManager.sol
+++ contracts/interfaces/ILpManager.sol
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-//import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-//import "../interfaces/IJoeFactory.sol";
-import "./IJoePair.sol";
-//import "../interfaces/IJoeRouter02.sol";
-//import "../interfaces/ILpManager.sol";
-import "hardhat/console.sol";
-
-
-interface ILpManager {
-
-    event SwapAndLiquify(uint256 indexed half, uint256 indexed initialBalance, uint256 indexed newRightBalance);
-    event SetAutomatedMarketMakerPair(address indexed pair, bool indexed value);
-
-    function afterTokenTransfer(address _sender) external returns (bool);
-
-    function swapAndLiquify(uint256 tokens) external returns (uint256);
-
-    function sendLPTokensTo(address to, uint256 tokens) external;
-
-    function createPairWith(address[2] memory path) external returns (IJoePair);
-
-    function swapLeftSideForRightSide(uint256 tokenAmount) external;
-
-    function addLiquidityToken(uint256 leftAmount, uint256 rightAmount) external;
-
-    function setAllowance(bool active) external;
-
-    function shouldLiquify(bool _liquifyEnabled) external;
-
-    function updateSwapTokensToLiquidityThreshold(uint256 _swapTokensToLiquidityThreshold) external;
-
-    function setFeeTo(address _newFeeAddress) external;
-
-    function getRouter() external view returns (address);
-
-    function getPair() external view returns (address);
-    
-    function getLeftSide() external view returns (address);
-
-    function getRightSide() external view returns (address);
-
-    function isPair(address _pair) external view returns (bool);
-
-    function isRouter(address _router) external view returns (bool);
-
-    function isLiquidityAdded() external view returns(bool);
-
-=======
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
@@ -65,7 +12,7 @@ interface ILpManager {
 
     function getRightSide() external view returns (address);
 
-    function getRewardAddress() external view returns(address);
+    function getRewardAddress() external view returns (address);
 
     function isPair(address _pair) external view returns (bool);
 
@@ -77,7 +24,5 @@ interface ILpManager {
 
     function isLiquidityAdded() external view returns (bool);
 
-    function buffer( ) external returns (bool);
-
->>>>>>> feature/lpManager
+    function buffer() external returns (bool);
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -19,13 +19,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
     solidity: {
-    version: "0.8.7",
     settings: {
       optimizer: {
         enabled: true,
         runs: 1
       }
-    }
+    },
+    compilers: [
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+    ],
   },
   networks: {
     ganache: {
