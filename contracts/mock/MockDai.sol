f92347d3c0c36b85a0c604c027e1fa5eda92945a
didoshotev <didoshotev@gmail.com>
2022-05-22 14:58:34 +0300

    MockDai file git issue

--- contracts/mock/MockDai.sol
+++ contracts/mock/MockDai.sol
@@ -1,16 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//Sole purpose of testing lp manager
-contract MockDai is ERC20, Ownable{
-    uint256 public _totalSupply = 200000*1e18;
-    
-    constructor() ERC20("Dai token","Dai"){
-        _mint(owner(), _totalSupply);
+contract MockDAI is ERC20 {
+    mapping(address => mapping(address => uint256)) public _allowances;
+
+    constructor() ERC20("Mock DAI", "MDAI") {}
+
+    function mintTokens(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+
+    function getBalance(address _balanceWanted) public view returns (uint256) {
+        return (balanceOf(_balanceWanted));
     }
 
 }
