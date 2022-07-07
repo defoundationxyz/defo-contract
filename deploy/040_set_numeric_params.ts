4f80ddc70f9350cac6a2e5fab67591de548922de
crypt0grapher7 <alexander@defoundation.xyz>
2022-07-07 13:41:50 -0400

    build:  numeric params and minor .env-example update

---
+++ deploy/040_set_numeric_params.ts
@@ -0,0 +1,48 @@
+import { DeployFunction } from "hardhat-deploy/types";
+
+import {
+  CHARITY_RATE,
+  DIAMOND_GEM,
+  LIQUIDITY_DAI_RATE,
+  LIQUIDITY_DEFO_RATE,
+  MINT_LIMIT_HOURS,
+  REWARD_TAX_TABLE,
+  REWARD_TIME,
+  RUBY_GEM,
+  SAPHIRE_GEM,
+  TREASURY_DAI_RATE,
+  TREASURY_DEFO_RATE,
+} from "../constants";
+import { OwnerFacet } from "../types";
+import { deployAnnounce, deploySuccess } from "../utils/helpers";
+
+const func: DeployFunction = async hre => {
+  const { getNamedAccounts, deployments } = hre;
+  const { deployer, donations, team, vault } = await getNamedAccounts();
+
+  const ownerFacetInstance = await hre.ethers.getContractAt<OwnerFacet>(
+    "OwnerFacet",
+    (
+      await deployments.get("DEFODiamond")
+    ).address,
+  );
+  deployAnnounce("\n\nInitializing OwnerFacet with numeric parameters from  constants/config.ts ...");
+
+  await ownerFacetInstance.setAddressAndDistTeam(team, TREASURY_DAI_RATE, TREASURY_DEFO_RATE);
+  await ownerFacetInstance.setAddressAndDistLiquidity(deployer, LIQUIDITY_DEFO_RATE, LIQUIDITY_DAI_RATE);
+  await ownerFacetInstance.setAddressDonation(donations, CHARITY_RATE);
+  await ownerFacetInstance.setAddressVault(vault);
+  await ownerFacetInstance.setMinRewardTime(REWARD_TIME);
+
+  await ownerFacetInstance.setRewardTax(REWARD_TAX_TABLE);
+  await ownerFacetInstance.setGemSettings("0", SAPHIRE_GEM);
+  await ownerFacetInstance.setGemSettings("1", RUBY_GEM);
+  await ownerFacetInstance.setGemSettings("2", DIAMOND_GEM);
+
+  // activate limit hours
+  await ownerFacetInstance.setMintLimitHours(MINT_LIMIT_HOURS);
+  deploySuccess(`Success`);
+};
+
+export default func;
+func.tags = ["DiamondInitialize"];
