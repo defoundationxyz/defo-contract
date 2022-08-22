df0abef174faa87e3fdc5d9ae702b158cac0c0ea
crypt0grapher <7blockchains@gmail.com>
2022-08-22 04:53:45 -0400

    test: adding more tests to reward facet and vault (DSC-125 scenario), vault tests for all the gem types supported

--- test/RewardsFacet/RewardsFacet.test.ts
+++ test/RewardsFacet/RewardsFacet.test.ts
@@ -1,8 +1,9 @@
-import { GEMS, GEM_TYPES_CONFIG, gemName } from "@config";
+import { GEMS, GEM_TYPES_CONFIG, fromWei, gemName } from "@config";
 import { RewardsFacet } from "@contractTypes/contracts/facets";
 import { getContractWithSigner } from "@utils/chain.helper";
 import { expect } from "chai";
 import newDebug from "debug";
+import { BigNumber } from "ethers";
 import hardhat, { deployments, ethers } from "hardhat";
 
 
@@ -10,6 +11,7 @@ const debug = newDebug("defo:YieldGemFacet.test.ts");
 
 describe("RewardsFacet", () => {
   let contract: RewardsFacet;
+  const testAmountToStake = (id: number) => GEM_TYPES_CONFIG[id].rewardAmountDefo as BigNumber;
 
   beforeEach(async () => {
     await deployments.fixture([
@@ -31,7 +33,18 @@ describe("RewardsFacet", () => {
     it("should earn reward with a given amount after one week", async () => {
       for (const i of Object.values(GEMS)) {
         debug(`getting reward amount of gem type ${gemName(i)}`);
-        expect(await contract.getRewardAmount(i)).to.be.equal(GEM_TYPES_CONFIG[i].rewardAmountDefo);
+        expect(await contract.getRewardAmount(i)).to.be.equal(testAmountToStake(i));
+      }
+    });
+    it("should show zero amount for the gems once staked", async () => {
+      for (const id of Object.values(GEMS)) {
+        debug(`getting reward amount of gem type ${gemName(id)}`);
+        await hardhat.run("vault", {
+          op: "stake",
+          id,
+          amount: Number(fromWei(testAmountToStake(id))),
+        });
+        expect(await contract.getRewardAmount(id)).to.be.equal(ethers.constants.Zero);
       }
     });
   });
@@ -49,7 +62,7 @@ describe("RewardsFacet", () => {
     it("should stake all earned reward to the vault", async () => {
       for (const i of Object.values(GEMS)) {
         debug(`staking the reward to the vault, complete amount ${gemName(i)}`);
-        expect(await contract.stakeReward(i, GEM_TYPES_CONFIG[i].rewardAmountDefo));
+        expect(await contract.stakeReward(i, testAmountToStake(i)));
         expect(await contract.getRewardAmount(i)).to.be.equal(ethers.constants.Zero);
       }
     });
--- test/VaultFacet/VaultFacet.test.ts
+++ test/VaultFacet/VaultFacet.test.ts
@@ -1,21 +1,25 @@
-import { DIAMOND_GEM, GEMS, HUNDRED_PERCENT, PROTOCOL_CONFIG, fromWei } from "@config";
+import { GEMS, GEM_TYPES_CONFIG, HUNDRED_PERCENT, PROTOCOL_CONFIG, fromWei, gemName } from "@config";
 import { VaultFacet } from "@contractTypes/contracts/facets";
 import { getContractWithSigner } from "@utils/chain.helper";
 import { expect } from "chai";
+import newDebug from "debug";
 import { BigNumber } from "ethers";
-import hardhat, { deployments } from "hardhat";
+import hardhat, { deployments, ethers } from "hardhat";
 
+
+const debug = newDebug("defo:VaultFacet.test.ts");
 
 describe("VaultFacet", () => {
   let contract: VaultFacet;
-  const TEST_GEM_ID = GEMS.diamond;
-  const TEST_AMOUNT_TO_STAKE = DIAMOND_GEM.rewardAmountDefo as BigNumber;
-  const TEST_AMOUNT_STAKED = TEST_AMOUNT_TO_STAKE.mul(
-    HUNDRED_PERCENT - <number>PROTOCOL_CONFIG.charityContributionRate,
-  ).div(HUNDRED_PERCENT);
-  const TEST_AMOUNT_UNSTAKED = TEST_AMOUNT_STAKED.mul(
-    HUNDRED_PERCENT - <number>PROTOCOL_CONFIG.vaultWithdrawalTaxRate,
-  ).div(HUNDRED_PERCENT);
+  const testAmountToStake = (id: number) => GEM_TYPES_CONFIG[id].rewardAmountDefo as BigNumber;
+  const testAmountStaked = (id: number) =>
+    testAmountToStake(id)
+      .mul(HUNDRED_PERCENT - <number>PROTOCOL_CONFIG.charityContributionRate)
+      .div(HUNDRED_PERCENT);
+  const testAmountUnStaked = (id: number) =>
+    testAmountStaked(id)
+      .mul(HUNDRED_PERCENT - <number>PROTOCOL_CONFIG.vaultWithdrawalTaxRate)
+      .div(HUNDRED_PERCENT);
 
   beforeEach(async () => {
     await deployments.fixture([
@@ -31,30 +35,53 @@ describe("VaultFacet", () => {
     await hardhat.run("permit");
     await hardhat.run("get-some-gems");
     await hardhat.run("jump-in-time");
-    await hardhat.run("vault", { op: "stake", id: TEST_GEM_ID, amount: Number(fromWei(TEST_AMOUNT_TO_STAKE)) });
+    for (const id of Object.values(GEMS)) {
+      debug(`minting gem ${gemName(id)}`);
+      await hardhat.run("vault", {
+        op: "stake",
+        id,
+        amount: Number(fromWei(testAmountToStake(id))),
+      });
+    }
   });
 
   describe("getStaked(uint256 _tokenId)", () => {
     it("should provide correct staked amount once staked", async () => {
-      expect(await contract.getStaked(TEST_GEM_ID)).to.be.equal(TEST_AMOUNT_STAKED);
+      for (const id of Object.values(GEMS)) {
+        debug(`testing ${gemName(id)}`);
+        expect(await contract.getStaked(id)).to.be.equal(testAmountStaked(id));
+      }
     });
   });
 
   describe("unStakeReward(uint256 _tokenId, uint256 _amount)", () => {
     it("should unstake all the amount staked earlier and emit event", async () => {
-      await expect(contract.unStakeReward(2, TEST_AMOUNT_STAKED))
-        .to.emit(contract, "UnStaked")
-        .withArgs(await contract.signer.getAddress(), TEST_AMOUNT_STAKED, TEST_AMOUNT_UNSTAKED);
+      for (const id of Object.values(GEMS)) {
+        debug(`testing ${gemName(id)}`);
+        await expect(contract.unStakeReward(id, testAmountStaked(id)))
+          .to.emit(contract, "UnStaked")
+          .withArgs(await contract.signer.getAddress(), testAmountStaked(id), testAmountUnStaked(id));
+      }
     });
   });
 
   describe("getTotalStaked()", () => {
     it("should return correct staked amount after staking", async () => {
-      expect(await contract.getTotalStaked()).to.be.equal(TEST_AMOUNT_STAKED);
+      expect(await contract.getTotalStaked()).to.be.equal(
+        Object.values(GEMS).reduce<BigNumber>(
+          (totalStaked, id) => totalStaked.add(testAmountStaked(id)),
+          ethers.constants.Zero,
+        ),
+      );
     });
     it("should return correct staked amount after staking and minting more gems", async () => {
       await hardhat.run("get-some-gems");
-      expect(await contract.getTotalStaked()).to.be.equal(TEST_AMOUNT_STAKED);
+      expect(await contract.getTotalStaked()).to.be.equal(
+        Object.values(GEMS).reduce<BigNumber>(
+          (totalStaked, id) => totalStaked.add(testAmountStaked(id)),
+          ethers.constants.Zero,
+        ),
+      );
     });
   });
 });
