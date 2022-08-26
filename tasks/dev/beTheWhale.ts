7a86a9e8a268a95db637d95e66bb23c9ec6ad3b0
crypt0grapher <7blockchains@gmail.com>
2022-08-26 16:09:37 -0400

    ci:  test coverage GitHub workflow action, add support of Fuji funding to beTheWhale.ts, minor README.md update

--- tasks/dev/beTheWhale.ts
+++ tasks/dev/beTheWhale.ts
@@ -2,12 +2,12 @@ import DAI_ABI from "abi/erc20-abi.json";
 import { Address } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-import { MAINNET_DAI_ADDRESS, MAINNET_DAI_WHALE_ADDRESS } from "../../constants/addresses";
+import { FUJI_DAI_ADDRESS, MAINNET_DAI_ADDRESS, MAINNET_DAI_WHALE_ADDRESS } from "../../constants/addresses";
 
 
 export const beTheWhale = async (hre: HardhatRuntimeEnvironment, accountToFund: Address, amountToTransfer?: number) => {
   if (hre.network.live) {
-    for (const token of [MAINNET_DAI_ADDRESS]) {
+    for (const token of [FUJI_DAI_ADDRESS]) {
       const contract = await hre.ethers.getContractAt(DAI_ABI, token);
       const toTransfer = hre.ethers.utils.parseEther(
         amountToTransfer ? amountToTransfer.toString() : (1e24).toString(),
