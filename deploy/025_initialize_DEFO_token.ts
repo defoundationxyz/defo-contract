1c940c14786ffd1523c3762a8faf75ea5dee9c48
crypt0grapher <7blockchains@gmail.com>
2022-09-06 04:15:21 -0400

    fix:  authorize facets to get rid of defo limit

--- deploy/025_initialize_DEFO_token.ts
+++ deploy/025_initialize_DEFO_token.ts
@@ -1,15 +1,18 @@
 import { DEFOToken } from "@contractTypes/contracts/token";
 import { getContractWithSigner } from "@utils/chain.helper";
-import { deploySuccess } from "@utils/output.helper";
+import { deployInfo, deploySuccess } from "@utils/output.helper";
 import { deployments } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 
-
 const func: DeployFunction = async hre => {
+  const { team, treasury, donations, vault, rewardPool } = await hre.getNamedAccounts();
   const defoContract = await getContractWithSigner<DEFOToken>(hre, "DEFOToken", "defoTokenOwner");
   const diamondDeployment = await deployments.get("DEFODiamond");
   await defoContract.linkDiamond(diamondDeployment.address);
-
+  for (const privilegedAddress of [diamondDeployment.address, team, treasury, donations, team, vault, rewardPool]) {
+    deployInfo(`Authorizing ${privilegedAddress}`);
+    await defoContract.rely(privilegedAddress);
+  }
   deploySuccess("DEFO Token initialized");
 };
 
