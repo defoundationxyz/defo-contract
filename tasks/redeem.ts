b823f9832bc9a5b309656a9ae7b1871df7556053
crypt0grapher <7blockchains@gmail.com>
2022-09-14 15:39:37 -0300

    build:  presold node contracts code for testing and verification

---
+++ tasks/redeem.ts
@@ -0,0 +1,5 @@
+// redeem script
+// 0. collect all the receivers of the nodes from mainnet
+// 1. sort these receivers, there would be 9 types: 3 gems, and 3 x 2 boosted ones
+// 2. mint in the loop, checking if the address is a presale gem holder, once minted - deactivate gems
+// Add separate task to deploy a test contract and a test node to check
