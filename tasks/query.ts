ed39e2b4e219835a90ed03b98f47f05c1dcfdfc6
crypt0grapher <7blockchains@gmail.com>
2022-10-26 14:36:08 +0800

    fix:  query task update - add numbers for vault amount, add silent param for automated background generation

--- tasks/query.ts
+++ tasks/query.ts
@@ -6,84 +6,87 @@ import assert from "assert";
 import chalk from "chalk";
 import { BigNumber } from "ethers";
 import Excel from "exceljs";
-import { task } from "hardhat/config";
+import { task, types } from "hardhat/config";
 import path from "path";
 
-task("query", "Get all the users with their balance, gems, and vault information.").setAction(async (_, hre) => {
-  const {
-    getNamedAccounts,
-    deployments,
-    ethers,
-    ethers: {
-      utils: { formatEther: fromWei },
-    },
-  } = hre;
-  const namedAccounts = await getNamedAccounts();
-  const { dai, forkedDefoToken } = namedAccounts;
-  const daiContract = await ethers.getContractAt(DAI_ABI, dai);
-  const defoTokenDeployment = (await deployments.getOrNull("DEFOToken"))?.address || "";
-  const diamondContract = await ethers.getContract<IDEFODiamond & ERC721EnumerableFacet>("DEFODiamond");
+task("query", "Get all the users with their balance, gems, and vault information.")
+  .addOptionalParam("silent", "true for silent output", false, types.boolean)
+  .setAction(async ({ silent }, hre) => {
+    const {
+      getNamedAccounts,
+      deployments,
+      ethers,
+      ethers: {
+        utils: { formatEther: fromWei },
+      },
+    } = hre;
+    const namedAccounts = await getNamedAccounts();
+    const { dai, forkedDefoToken } = namedAccounts;
+    const daiContract = await ethers.getContractAt(DAI_ABI, dai);
+    const defoTokenDeployment = (await deployments.getOrNull("DEFOToken"))?.address || "";
+    const diamondContract = await ethers.getContract<IDEFODiamond & ERC721EnumerableFacet>("DEFODiamond");
 
-  const workbook = new Excel.Workbook();
-  const worksheet = workbook.addWorksheet("DeFo Users");
-  const columns = [
-    { key: "address", header: "User" },
-    { key: "avax", header: "Avax" },
-    { key: "dai", header: "Dai" },
-    { key: "defo", header: "DEFO" },
-    { key: "gems", header: "Gems" },
-    { key: "vault", header: "DEFO in Vault" },
-  ];
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet("DeFo Users");
+    const columns = [
+      { key: "address", header: "User" },
+      { key: "avax", header: "Avax" },
+      { key: "dai", header: "Dai" },
+      { key: "defo", header: "DEFO" },
+      { key: "gems", header: "Gems" },
+      { key: "vault", header: "DEFO in Vault" },
+    ];
 
-  worksheet.columns = columns;
+    worksheet.columns = columns;
 
-  const totalSupply = await diamondContract.totalSupply();
-  info(`\n 📡 Total ${totalSupply} gems minted. Querying details...`);
-  if (totalSupply.isZero()) throw new Error("No gems minted");
+    const totalSupply = await diamondContract.totalSupply();
+    if (!silent) info(`\n 📡 Total ${totalSupply} gems minted. Querying details...`);
+    if (totalSupply.isZero()) throw new Error("No gems minted");
 
-  const gemIds = await Promise.all(
-    [...Array(totalSupply.toNumber()).keys()].map(async i => await diamondContract.tokenByIndex(i)),
-  );
+    const gemIds = await Promise.all(
+      [...Array(totalSupply.toNumber()).keys()].map(async i => await diamondContract.tokenByIndex(i)),
+    );
 
-  const users = new Set<string>();
-  for (const id of gemIds) {
-    users.add(await diamondContract.ownerOf(id));
-  }
+    const users = new Set<string>();
+    for (const id of gemIds) {
+      users.add(await diamondContract.ownerOf(id));
+    }
 
-  const defoContract =
-    forkedDefoToken || defoTokenDeployment
-      ? await ethers.getContractAt("DEFOToken", forkedDefoToken || defoTokenDeployment)
-      : null;
-  await networkInfo(hre, info);
-  assert(defoContract, "defoContract is null");
-  announce(
-    `DEFO token is ${chalk.yellow(
-      forkedDefoToken ? "on live network" : defoTokenDeployment ? "deployed locally" : chalk.red("not deployed!"),
-    )}. Address: ${defoContract.address}`,
-  );
-
-  const table = await Promise.all(
-    Array.from(users).map(async accountAddress => {
-      const userGems = (await diamondContract.getGemIdsOf(accountAddress)).map(gemId => gemId.toNumber());
-      const stakedForGems = await Promise.all(userGems.map(gemId => diamondContract.getStaked(gemId)));
-      const vault = stakedForGems.reduce<BigNumber>(
-        (totalStaked, stakedForGem) => totalStaked.add(stakedForGem),
-        ethers.constants.Zero,
+    const defoContract =
+      forkedDefoToken || defoTokenDeployment
+        ? await ethers.getContractAt("DEFOToken", forkedDefoToken || defoTokenDeployment)
+        : null;
+    if (!silent) await networkInfo(hre, info);
+    assert(defoContract, "defoContract is null");
+    if (!silent)
+      announce(
+        `DEFO token is ${chalk.yellow(
+          forkedDefoToken ? "on live network" : defoTokenDeployment ? "deployed locally" : chalk.red("not deployed!"),
+        )}. Address: ${defoContract.address}`,
       );
-      const data = {
-        address: accountAddress,
-        avax: Number(Number(fromWei(await ethers.provider.getBalance(accountAddress))).toFixed(3)),
-        dai: Number(Number(fromWei(await daiContract.balanceOf(accountAddress))).toFixed(3)),
-        defo: defoContract && Number(Number(fromWei(await defoContract.balanceOf(accountAddress))).toFixed(3)),
-        gems: Number(await diamondContract.balanceOf(accountAddress)),
-        vault: fromWei(vault),
-      };
-      worksheet.addRow(data);
-      return data;
-    }),
-  );
-  console.table(table);
-  const exportPath = path.resolve(__dirname, "query.xlsx");
 
-  await workbook.xlsx.writeFile(exportPath);
-});
+    const table = await Promise.all(
+      Array.from(users).map(async accountAddress => {
+        const userGems = (await diamondContract.getGemIdsOf(accountAddress)).map(gemId => gemId.toNumber());
+        const stakedForGems = await Promise.all(userGems.map(gemId => diamondContract.getStaked(gemId)));
+        const vault = stakedForGems.reduce<BigNumber>(
+          (totalStaked, stakedForGem) => totalStaked.add(stakedForGem),
+          ethers.constants.Zero,
+        );
+        const data = {
+          address: accountAddress,
+          avax: Number(Number(fromWei(await ethers.provider.getBalance(accountAddress))).toFixed(3)),
+          dai: Number(Number(fromWei(await daiContract.balanceOf(accountAddress))).toFixed(3)),
+          defo: defoContract && Number(Number(fromWei(await defoContract.balanceOf(accountAddress))).toFixed(3)),
+          gems: Number(await diamondContract.balanceOf(accountAddress)),
+          vault: Number(fromWei(vault)),
+        };
+        worksheet.addRow(data);
+        return data;
+      }),
+    );
+    if (!silent) console.table(table);
+    const exportPath = path.resolve(__dirname, "query.xlsx");
+
+    await workbook.xlsx.writeFile(exportPath);
+  });
